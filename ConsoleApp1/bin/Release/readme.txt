d0247e27fb83064effea3bcb19d127ed2736fec9
Tony Hoang <hoangtrongminh304@gmail.com>
2017-08-02 22:01:39 +0700

    Add readme

---
+++ ConsoleApp1/bin/Release/readme.txt
@@ -0,0 +1,4 @@
+PinsTool.exe <s32sdk_path> <cpu>
+
+s32sdk_path = "d:\04_Projects\ASDK-S32_SDK\sdk_codebase"
+cpu = [S32K148_100_BGA|S32K144_64|...]
---
+++ ConsoleApp1/bin/Release/run.bat
@@ -0,0 +1,5 @@
+set s32sdk_path=d:\04_Projects\ASDK-S32_SDK\sdk_codebase
+
+PinsTool.exe %s32sdk_path% S32K148_100_BGA
+PinsTool.exe %s32sdk_path% S32K144_64
+
